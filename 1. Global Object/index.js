c4399cf1f7992c5b03ff5626f22f29e98a21c81b
Anupam Ghosh <65004586+ghoshanupam04@users.noreply.github.com>
2024-02-24 20:04:11 +0530

    Add files via upload

---
+++ 1. Global Object/index.js
@@ -0,0 +1,12 @@
+/*
+Global Function: 
+Global Function or Global Object are that Object which are also run in the Browser or our Code Compiler 
+that function or objects are also colled Global Function. 
+*/
+
+//Example:
+setTimeout(()=>{
+    console.log('Sorry For Wait 3-Second ');
+},3000)
+
+console.log(global)// Print all Global Function which support our Code Compiler 
