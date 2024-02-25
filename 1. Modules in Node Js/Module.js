635bc83611ee03301c130fd594a8fc6515864b50
Anupam Ghosh <65004586+ghoshanupam04@users.noreply.github.com>
2024-02-25 20:45:48 +0530

    Add files via upload

---
+++ 1. Modules in Node Js/Module.js
@@ -0,0 +1,11 @@
+function add(a,b){  //create a function
+    return a+b;
+}
+function sub(a,b){  //create a function
+    return a-b;
+}
+
+module.exports={    //export the file
+    addthenumber:add, subthenumber:sub,
+} 
+
---
+++ 1. Modules in Node Js/index.js
@@ -0,0 +1,3 @@
+const math=require('./Module')
+console.log("The math value is: "+math.addthenumber(4,5)); 
+console.log("The math value is: "+math.subthenumber(5,4));
